18a48496babd4534cd9dcb704e19d2bf83fcf116
TalAter <tal@talater.com>
2019-12-17 01:10:03 +0200

    Use `padding` and simpler drawing (points) in `1d-noise`

--- 1d-noise/js/main.js
+++ 1d-noise/js/main.js
@@ -1,18 +1,15 @@
 const width = 640;
 const height = 480;
+const padding = 30;
 let t = 0;
 
 // const getValue = (i, min, max) => random(min, max);
-const getValue = (i, min, max) => min + noise(i / 500, t / 500) * max;
+const getValue = (d1, d2, min, max) => min + noise(d1 / 250, d2 / 250) * max;
 
 const chartValues = () => {
   stroke(150);
-  const points = [];
-  for (let i = 20; i < width - 20; i++) {
-    points.push(createVector(i, getValue(i, 20, height - 20)));
-  }
-  for (let i = 1; i < points.length; i++) {
-    line(points[i - 1].x, points[i - 1].y, points[i].x, points[i].y);
+  for (let x = padding; x < width - padding; x++) {
+    point(x, getValue(x, t, padding, height - padding));
   }
 };
 
