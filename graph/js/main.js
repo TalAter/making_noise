436ae697766a0c8f474417b45af5631135242335
TalAter <tal@talater.com>
2019-12-16 22:21:16 +0200

    One dimensional noise charted

--- graph/js/main.js
+++ graph/js/main.js
@@ -1,7 +1,27 @@
+const width = 640;
+const height = 480;
+let t = 0;
+
+// const getValue = (i, min, max) => random(min, max);
+const getValue = (i, min, max) => min + noise(i / 500, t / 500) * max;
+
+const chartValues = () => {
+  stroke(150);
+  const points = [];
+  for (let i = 20; i < width - 20; i++) {
+    points.push(createVector(i, getValue(i, 20, height - 20)));
+  }
+  for (let i = 1; i < points.length; i++) {
+    line(points[i - 1].x, points[i - 1].y, points[i].x, points[i].y);
+  }
+};
+
 setup = function setup() {
-  createCanvas(640, 480);
+  createCanvas(width, height);
 };
 
 draw = function draw() {
-  ellipse(50, 50, 80, 80);
+  background(0);
+  chartValues();
+  t++;
 };
