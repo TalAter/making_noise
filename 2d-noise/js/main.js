44bd7888d6a9f0ce17acada9857dfe7118e35e56
TalAter <tal@talater.com>
2019-12-17 01:16:03 +0200

    Rename variable in `2d-noise` for consistency with `1d-noise`

--- 2d-noise/js/main.js
+++ 2d-noise/js/main.js
@@ -1,7 +1,7 @@
 const width = 300;
 const height = 300;
 const padding = 30;
-let z = 0;
+let t = 0;
 
 // const getValue = (min, max) => random(min, max);
 const getValue = (min, max, d1, d2, d3) => map(noise(d1 / 250, d2 / 250, d3 / 120), 0, 1, min, max);
@@ -9,7 +9,7 @@ const getValue = (min, max, d1, d2, d3) => map(noise(d1 / 250, d2 / 250, d3 / 12
 const chartValues = () => {
   for (let x = padding; x < width - padding; x++) {
     for (let y = padding; y < height - padding; y++) {
-      set(x, y, color(getValue(0, 255, x, y, z)));
+      set(x, y, color(getValue(0, 255, x, y, t)));
     }
   }
   updatePixels();
@@ -29,5 +29,5 @@ draw = function draw() {
   background(0);
   chartValues();
   drawFPS();
-  z++;
+  t++;
 };
